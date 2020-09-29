85151c8eb7a1f61eb2bdda463778f46fefdfa444
Jason Lu <jasonlu2907@gmail.com>
2020-09-29 11:36:58 -0500

    Lil adjust http method

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -20,23 +20,24 @@ export class ItemsService {
     // this.http is a HttpClient library provide by @angular/common
     // we are calling .get() method over this.http object
     // this .get() method takes URL to call API
-    return this.http.get(`https://cors-anywhere.herokuapp.com/${this.URL}`);
+    // https://cors-anywhere.herokuapp.com/
+    return this.http.get(`${this.URL}`);
   }
 
-  // create a method named: resolveItems()
-  // this method returns list-of-items in form of Observable
-  // every HTTTP call returns Observable object
-  // createItem(): Observable<any> {
-  //   console.log('Request is sent!');
-  //   // Using the POST method
-  //   const headers =  {
-  //     headers: new  HttpHeaders({ 
-  //       'Content-Type': 'application/x-www-form-urlencoded'})
-  //   };
-  //   return this.http.post(this.URL,
-  //   {
-  //     'email' : 'ankit.codechintan@gmail.com',
-  //     'phone' : 910950
-  //   },headers)
-  // }
+  // create a method named: createItems()
+  //this method returns list-of-items in form of Observable
+  //every HTTTP call returns Observable object
+  /* createItem(): Observable<any> {
+    console.log('Request is sent!');
+    // Using the POST method
+    const headers =  {
+      headers: new  HttpHeaders({ 
+        'Content-Type': 'application/x-www-form-urlencoded'})
+    };
+    return this.http.post(this.URL,
+    {
+      'email' : 'ankit.codechintan@gmail.com',
+      'phone' : 910950
+    },headers)
+   } */
 }
