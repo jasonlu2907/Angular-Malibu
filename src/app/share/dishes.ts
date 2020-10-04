d88d775d7853b4450a7c674d6e20cf8d3449d9c4
Jason Lu <jasonlu2907@gmail.com>
2020-10-04 17:30:17 -0500

    Lil data binding 2

--- src/app/share/dishes.ts
+++ src/app/share/dishes.ts
@@ -35,12 +35,6 @@ export const DISHES: Dish[] = [
                 comment: 'Ultimate, Reaching for the stars!',
                 author: 'Ringo Starry',
                 date: '2013-12-02T17:57:28.556094Z'
-            },
-            {
-                rating: 2,
-                comment: 'It\'s your birthday, we\'re gonna party!',
-                author: '25 Cent',
-                date: '2011-12-02T17:57:28.556094Z'
             }
         ]
     },
@@ -54,12 +48,6 @@ export const DISHES: Dish[] = [
         price: '1.99',
         description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
         comments: [
-            {
-                rating: 5,
-                comment: 'Imagine all the eatables, living in conFusion!',
-                author: 'John Lemon',
-                date: '2012-10-16T17:57:28.556094Z'
-            },
             {
                 rating: 4,
                 comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
@@ -101,12 +89,6 @@ export const DISHES: Dish[] = [
                 comment: 'Imagine all the eatables, living in conFusion!',
                 author: 'John Lemon',
                 date: '2012-10-16T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Sends anyone to heaven, I wish I could get my mother-in-law to eat it!',
-                author: 'Paul McVites',
-                date: '2014-09-05T17:57:28.556094Z'
             },
             {
                 rating: 3,
@@ -155,12 +137,6 @@ export const DISHES: Dish[] = [
                 comment: 'Eat it, just eat it!',
                 author: 'Michael Jaikishan',
                 date: '2015-02-13T17:57:28.556094Z'
-            },
-            {
-                rating: 4,
-                comment: 'Ultimate, Reaching for the stars!',
-                author: 'Ringo Starry',
-                date: '2013-12-02T17:57:28.556094Z'
             },
             {
                 rating: 2,
